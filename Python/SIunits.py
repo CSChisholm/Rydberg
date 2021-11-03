c33d2c160d10fb317f301e64040dbd5bc6a90329
Craig Chisholm <craig.chisholm@icfo.eu>
2021-11-03 15:37:12 +0100

    Use scipy.constants for more accurate numbers

--- Python/SIunits.py
+++ Python/SIunits.py
@@ -9,14 +9,16 @@ Created on Wed May 15 17:16:25 2019
 #Script to load various SI values which are needed to convert back from
 #atomic rydberg units
 
-finestrucconst = 1/137 #Fine structure constant
+import scipy.constants as constants
+
+finestrucconst = constants.alpha #Fine structure constant
 atomenergy = 27.2 #Atomic energy units in eV
 Ry = 1
-rpceV = 6.58211889e-16 #Reduced Planck constant in eV s
-rpcJ = 1.054571596e-34 #Reduced Planck constant in J s
-lightc = 2.99792458e8 #Speed of light in m/s
-vacpmtvty = 8.854187817e-12 #Vacuum permitivity in F/m
+rpcJ = constants.hbar #Reduced Planck constant in J s
+lightc = constants.c #Speed of light in m/s
+vacpmtvty = constants.epsilon_0 #Vacuum permitivity in F/m
 espin = 0.5 #electron spin
-boltzconst = 1.3807e-23 #Boltzmann's constant in J/K
-bohrrad = 0.5291772083e-10 #Bohr radius
-eleccharge = 1.602176462e-19 #electron charge
+boltzconst = constants.k #Boltzmann's constant in J/K
+bohrrad = constants.physical_constants['Bohr radius'][0] #Bohr radius
+eleccharge = constants.e #electron charge
+rpceV = constants.hbar/constants.e #Reduced Planck constant in eV s
